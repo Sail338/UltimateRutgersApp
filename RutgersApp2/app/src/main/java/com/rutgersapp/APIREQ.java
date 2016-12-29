e99222891b7ada1c41004a62ca89ca7d8941ab1c
Sail338 <dude.ex001@gmail.com>
2016-12-28 19:03:49 -0500

    inital commit

---
+++ RutgersApp2/app/src/main/java/com/rutgersapp/APIREQ.java
@@ -0,0 +1,10 @@
+package com.rutgersapp;
+
+/**
+ * Created by hari on 12/28/16.
+ */
+public class APIREQ {
+        public static final String base = "https://rumobile.rutgers.edu";
+        public  static final String APIVERSION = "2";
+        public static final String activestops = "nbactivestops.txt";
+}
