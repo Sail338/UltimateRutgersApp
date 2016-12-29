f3f0f9f0ad6d0aa33c06ad991f8b77de7042e853
Sail338 <dude.ex001@gmail.com>
2016-12-28 20:10:13 -0500

    Added method to pull active busses

--- RutgersApp2/app/src/main/java/com/rutgersapp/APIREQ.java
+++ RutgersApp2/app/src/main/java/com/rutgersapp/APIREQ.java
@@ -1,10 +1,39 @@
 package com.rutgersapp;
 
+
+import okhttp3.*;
+
+import java.io.IOException;
+
 /**
  * Created by hari on 12/28/16.
  */
 public class APIREQ {
-        public static final String base = "https://rumobile.rutgers.edu";
-        public  static final String APIVERSION = "2";
+
+        public static final String base = "https://rumobile.rutgers.edu/";
+        public  static final String APIVERSION = "2/";
         public static final String activestops = "nbactivestops.txt";
+
+        public  void getActiveStops(final ResponseValue value) throws IOException {
+            OkHttpClient client = new OkHttpClient();
+            String reponsestr;
+             Request request = new Request.Builder().url(base+APIVERSION+activestops).build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    //throw an Error
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if(response.isSuccessful()){
+
+                        value.onResponse(response.body().string());
+                    }
+
+                }
+            });
+
+        }
+
 }
--- RutgersApp2/app/src/main/java/com/rutgersapp/MainActivity.java
+++ RutgersApp2/app/src/main/java/com/rutgersapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.rutgersapp;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        APIREQ apireq = new APIREQ();
+        try {
+            String resp;
+            apireq.getActiveStops(new ResponseValue() {
+                @Override
+                public void onResponse(Object value) {
+                    Log.d("RETURND", value.toString());
+                }
+            });
+
+        } catch (IOException e){
+            Log.e("ERR","BIG ERR");
+        }
     }
 
 }
