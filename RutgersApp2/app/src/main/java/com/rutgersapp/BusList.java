8c2d0c676e995bc530eeee6f25910fb5b4f7fd8c
Sail338 <dude.ex001@gmail.com>
2016-12-30 14:36:23 -0500

    Started to remove unused XML

--- RutgersApp2/app/src/main/java/com/rutgersapp/BusList.java
+++ RutgersApp2/app/src/main/java/com/rutgersapp/BusList.java
@@ -3,18 +3,17 @@ package com.rutgersapp;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
-import android.util.Xml;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlSerializer;
+
 
 
 import java.util.ArrayList;
-import java.util.logging.XMLFormatter;
+
 
 /**
  * Created by hari on 12/30/16.
