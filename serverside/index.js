243f710352bb398db7015eb77e6600857100b928
Sail338 <srihari.shankar1@gmail.com>
2017-01-11 19:03:54 -0500

    started the methods to grab data

---
+++ serverside/index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var http = require('http')
+var app = express()
+
+app.get('/sniper',function(req,res) {(
+	//create get request based on params from sent requesrt
+	//grab the params
+	//
+	
+});
